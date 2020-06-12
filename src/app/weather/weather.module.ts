ccdc71f0676795c9dccdcf7ee9a5e02a79ba5ed2
candidJ <jaskirat19mann@gmail.com>
2020-06-13 02:45:12 +0300

    adding input search for weather

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForecastComponent } from './forecast/forecast.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -12,7 +13,8 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([{
       path: "",
       component: ForecastComponent
-    }])
+    }]),
+    SharedModule
   ]
 })
 export class WeatherModule { }
