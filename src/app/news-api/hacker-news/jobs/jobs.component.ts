e29af58aabd3164075d85a50ead69c8bc6587202
candidJ <jaskirat19mann@gmail.com>
2020-07-06 12:17:35 +0300

    feat: using inheritance to show hacker jobs

--- src/app/news-api/hacker-news/jobs/jobs.component.ts
+++ src/app/news-api/hacker-news/jobs/jobs.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FeedComponent } from '../feed/feed.component';
+import { HackerNewsApiService } from '../hacker-news-api.service';
 
 @Component({
   selector: 'app-jobs',
-  templateUrl: './jobs.component.html',
+  templateUrl: '../feed/feed.component.html',
   styleUrls: ['./jobs.component.scss']
 })
-export class JobsComponent implements OnInit {
-
-  constructor() { }
+export class JobsComponent extends FeedComponent implements OnInit {
+  constructor(hackerService: HackerNewsApiService) {
+    super(hackerService);
+  }
 
   ngOnInit(): void {
+    super.ngOnInit();
   }
 
 }
