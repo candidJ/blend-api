8d0c2f4f881cbbfe40a4b357141ca80b1a007bb4
candidJ <jaskirat19mann@gmail.com>
2021-06-05 14:48:18 +0530

    feat: compute on equate n operator chaining

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -14,49 +14,58 @@ export class CalculatorComponent implements OnInit {
   previousInput: string = "";
   operation: string = "";
   currentOperator: string = "";
+
+  private isComputationDone: boolean = false;
+
   constructor() { }
 
   private onlyAllowOneDecimal(userInput: ICalculatorLayout) {
     if (userInput.value === '.' && this.output.includes('.')) {
       return;
     } else {
-      this.output += userInput.value;
+      if (this.isComputationDone) {
+        this.output = userInput.value
+      } else {
+        this.output += userInput.value;
+      }
     }
   }
 
   private compute() {
+    let previousInput = parseFloat(this.previousInput);
+    let currentOutput = parseFloat(this.output);
+
     switch (this.currentOperator) {
       case "+":
-        this.output =   (parseFloat(this.previousInput) + parseFloat(this.output)).toFixed(2).toString();
+        this.output = (previousInput + currentOutput).toString();
         break;
       case "-":
-        this.output =   (parseFloat(this.previousInput) - parseFloat(this.output)).toFixed(2).toString();
+        this.output = (previousInput - currentOutput).toString();
         break;
       case "/":
-        this.output =   (parseFloat(this.previousInput) / parseFloat(this.output)).toFixed(2).toString();
+        this.output = (previousInput / currentOutput).toString();
         break;
       case "*":
-        this.output =   (parseFloat(this.previousInput) * parseFloat(this.output)).toFixed(2).toString();
+        this.output = (previousInput * currentOutput).toString();
+        break;
+      case "%":
+        this.output = (previousInput % currentOutput).toString();
         break;
       default:
         break;
     }
     console.log(this.output, "this.output");
+    this.clear();
+    this.isComputationDone = true;
   }
 
   private appendOperator(userInput: ICalculatorLayout) {
-    this.operation = `${this.output}${userInput.viewValue}`;
-    this.currentOperator = userInput.value;
-    this.previousInput = this.output;
-    this.output = "";
-  }
-
-  private checkForOperation(userInput: ICalculatorLayout) {
-    if (this.operation.length > 0) {
-      this.operation = (`${this.operation}${userInput.value}`);
-      this.output += userInput.value;
-    } else {
-      this.onlyAllowOneDecimal(userInput);
+    if (!(this.calculatorConfig.some(cc => this.operation.includes(cc.viewValue)))) {
+      this.isComputationDone = false;
+      this.operation = `${this.output}${userInput.viewValue}`;
+      this.currentOperator = userInput.value;
+      this.previousInput = this.output;
+      this.output = "";
     }
   }
 
@@ -66,16 +75,31 @@ export class CalculatorComponent implements OnInit {
     this.currentOperator = "";
   }
 
+  private removeLastDigit() {
+    if (this.output.length > 0) {
+      this.output = this.output.toString().slice(0, -1);
+    }
+  }
+
   public onUserInput(userInput: ICalculatorLayout) {
     switch (userInput.className) {
       case "numeric":
-        this.checkForOperation(userInput);
+        this.onlyAllowOneDecimal(userInput);
         break;
       case "operator":
         this.appendOperator(userInput);
         break;
       case "equate":
         this.compute();
+        break;
+      case "ac":
+        this.clear();
+        this.output = "";
+        break;
+      case "del":
+        this.removeLastDigit();
+        break
+      default:
         this.clear();
         break;
     }
