d23af0a354659f47369462367850d41a393895ed
Jaskirat Mann <jaskirat_mann@outlook.com>
2023-10-20 14:50:44 -0700

    chore: [nx migration] update-configs-jest-29

--- libs/hacker-news/jest.config.ts
+++ libs/hacker-news/jest.config.ts
@@ -3,15 +3,16 @@ export default {
   displayName: 'hacker-news',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../coverage/libs/hacker-news',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
