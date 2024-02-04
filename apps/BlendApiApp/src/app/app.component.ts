1898beebc1ed507dd80f7cbf5b81bf129aa9e363
Jaskirat Mann <jaskirat_mann@outlook.com>
2024-02-04 12:07:09 -0800

    refactor: standalone-app-module

--- apps/BlendApiApp/src/app/app.component.ts
+++ apps/BlendApiApp/src/app/app.component.ts
@@ -4,11 +4,22 @@ import {
   NavbarMenu,
   NavbarMenuItems,
 } from '@blend-api/shared';
+import { NotificationsModule } from '../../../../libs/shared/src/lib/modules/notifications/notifications.module';
+import { LoaderModule } from '../../../../libs/shared/src/lib/modules/loader/loader.module';
+import { RouterOutlet } from '@angular/router';
+import { NavbarModule } from '../../../../libs/shared/src/lib/modules/navbar/navbar.module';
 
 @Component({
-  selector: 'ba-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'ba-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [
+        NavbarModule,
+        RouterOutlet,
+        LoaderModule,
+        NotificationsModule,
+    ],
 })
 export class AppComponent {
   title = 'Blend API';
--- apps/BlendApiApp/src/main.ts
+++ apps/BlendApiApp/src/main.ts
@@ -1,13 +1,20 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { NavbarModule, LoaderModule, IconsModule, NotificationsModule } from '@blend-api/shared';
+import { AppRoutingModule } from './app/app-routing.module';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule, { preserveWhitespaces: false })
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, NavbarModule, LoaderModule, IconsModule, NotificationsModule),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch((err) => console.error(err));
