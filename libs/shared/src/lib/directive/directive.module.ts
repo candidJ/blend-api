e0e0831ea7f90dd3ade4dd9c93763c3b90be4a75
Jaskirat Mann <jaskirat_mann@outlook.com>
2024-02-04 13:14:54 -0800

    refactor: prune directive module

--- libs/shared/src/lib/directive/directive.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { HighlightedDirective } from '../directive/highlighted.directive';
-
-@NgModule({
-  imports: [CommonModule, HighlightedDirective],
-  exports: [HighlightedDirective],
-})
-export class DirectiveModule {}
--- libs/shared/src/lib/directive/index.ts
+++ libs/shared/src/lib/directive/index.ts
@@ -1,2 +1,1 @@
-export * from './directive.module';
 export * from './highlighted.directive';
