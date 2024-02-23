69c6811fe385c41bcc5e5a95c76cc7ae66aed71e
Jaskirat Mann <jaskirat_mann@outlook.com>
2024-02-23 15:16:02 -0800

    refactor: loader icon

--- libs/shared/src/lib/modules/loader/components/loader.component.ts
+++ libs/shared/src/lib/modules/loader/components/loader.component.ts
@@ -1,12 +1,13 @@
 import { Component, Directive, inject } from '@angular/core';
 import { LoaderService } from '../services/loader.service';
 import { AsyncPipe } from '@angular/common';
+import { IconsModule } from '../../feather-icons/icons.module';
 @Component({
   selector: 'ba-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss'],
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, IconsModule],
 })
 export class LoaderComponent {
   readonly loaderService: LoaderService = inject(LoaderService);
